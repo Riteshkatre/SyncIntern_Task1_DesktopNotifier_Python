83740f5a1aa8c220bf9687510bde7cc679ecffe3
Riteshkatre <123712880+Riteshkatre@users.noreply.github.com>
2023-03-03 00:07:20 +0530

    Add files via upload

---
+++ notifier.py
@@ -0,0 +1,7 @@
+from plyer import notification
+
+notification.notify(
+    title="08:00 O'clock ",
+    message=' wake up early it is time you have to go to college',
+    timeout=10 #duration in seconds
+)
